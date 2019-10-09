c6b2ba50d038be04191ce2ff3177325bea9a3655
lain <lain@soykaf.club>
2019-10-09 10:50:14 +0200

    Migrations: Drop the object id index on jsonb.

---
+++ priv/repo/migrations/20191008111631_drop_object_id_json_index.exs
@@ -0,0 +1,7 @@
+defmodule Pleroma.Repo.Migrations.DropObjectIdJsonIndex do
+  use Ecto.Migration
+
+  def change do
+    drop_if_exists unique_index(:objects, ["(data->>'id')"], name: :objects_unique_apid_index)
+  end
+end
