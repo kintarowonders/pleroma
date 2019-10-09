7fd29bd0b8e6f6b6bd5ef117c65cc1e65f05efee
lain <lain@soykaf.club>
2019-10-09 10:51:24 +0200

    Migrations: Fill the object_ap_id field in activities.

---
+++ priv/repo/migrations/20191008113641_fill_activity_object_ap_id_field.exs
@@ -0,0 +1,7 @@
+defmodule Pleroma.Repo.Migrations.FillActivityObjectApIdField do
+  use Ecto.Migration
+
+  def change do
+    execute("update activities set object_ap_id = COALESCE(data->'object'->>'id', data->>'object')")
+  end
+end
